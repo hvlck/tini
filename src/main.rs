0a1fc3525c4bb23373fda55f1290714f69c49dc4
EthanJustice <47954690+EthanJustice@users.noreply.github.com>
2020-10-03 19:05:06 -0400

    feat: initial arg parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+// std
+
+// external
+use clap::{App, Arg, SubCommand};
+
 fn main() {
-    println!("Hello, world!");
+    let app = App::new("tini")
+        .version(env!("CARGO_PKG_VERSION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .about(env!("CARGO_PKG_DESCRIPTION"))
+        .subcommand(
+            SubCommand::with_name("generate")
+                .about("Generate tinysearch index.")
+                .version(env!("CARGO_PKG_VERSION"))
+                .author(env!("CARGO_PKG_AUTHORS"))
+                .arg(Arg::with_name("INPUT").help("Directory to index.")),
+        )
+        .get_matches();
+
+    if let Some(_v) = app.subcommand_matches("generate") {
+        println!("Called generate.");
+    }
 }
